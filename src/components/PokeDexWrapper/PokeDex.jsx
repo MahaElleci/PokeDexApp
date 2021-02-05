77c13263a31fe7090b6f272f5d90e63c74f41b8d
Maha Elleci <maha.elleci@itworx.com>
2021-02-05 22:37:02 +0200

    added deployment configs on gh-pages, handled card hover state change

--- src/components/PokeDexWrapper/PokeDex.jsx
+++ src/components/PokeDexWrapper/PokeDex.jsx
@@ -15,10 +15,10 @@ const PokeDex = () => {
         <img src="https://cdn.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png" />
       </div> 
       {state.isLoading && <img className="loader" src={loader} />}
-      <div className="pokeDex-wrapper__cards"> 
+      <div className="pokeDex-wrapper__cards">  
           { state.pokemons.map((item, index) => {
             return <PokemonCard key={index} item={item} />;
-          })}
+          })} 
       </div>
     </div>
   );
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -3,13 +3,23 @@ import { getPokemonData } from "../../overmind/effects";
 import "./styles.scss";
 
 const PokemonCard = ({ item }) => {
-  const [cardData, setCardData] = useState({}); 
+  const [cardData, setCardData] = useState({});
+  const [isHovering, setIsHovering] = useState(false);
   useEffect(async () => {
     const data = await getPokemonData(item.url);
     setCardData(data);
   }, [cardData]);
+
+
+  const handleMouseHover = () => {
+    setIsHovering(!isHovering);
+  };
   return (
-    <div className="pokemonCard">
+    <div
+      onMouseEnter={() => handleMouseHover()}
+      onMouseLeave={() => handleMouseHover()}
+      className="pokemonCard"
+    >
       <div className="pokemonCard__heading">
         <div className="imagePlaceholder">
           <img src="https://cache.desktopnexus.com/thumbseg/1327/1327833-bigthumbnail.jpg" />
